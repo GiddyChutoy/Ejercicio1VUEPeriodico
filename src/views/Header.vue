702f2418faa25a34b70e234e78e68852677d352b
Adrian Mateos <adrian@adrian>
2021-02-01 13:07:25 +0100

    [ADD] Empezamos el ejercicio de VUE Router

---
+++ src/views/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <h1>THE WORLD</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Header',
+  components: {
+    
+  }
+}
+</script>
+
+
+<style scoped>
+  div{
+    background-color: grey;
+    color: black;
+  }
+</style>
